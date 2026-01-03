57fcdb05b992e907a208a12e9ce2a12aff1fdc57
Sjay <sjay07059@gmail.com>
2026-01-03 08:57:00 +0100

    Video Fix

--- pages/Learning.tsx
+++ pages/Learning.tsx
@@ -49,7 +49,7 @@ interface LearningProps {
 
 const getYouTubeId = (url: string) => {
     if (!url) return null;
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 };
